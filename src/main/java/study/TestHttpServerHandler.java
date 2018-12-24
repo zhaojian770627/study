1cc4d36129155f7adb670e8ba8938120264c10da
zhaojianc <zhaojianc@yonyou.com>
2018-12-24 09:32:05 +0800

    up
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- src/main/java/study/TestHttpServerHandler.java
+++ src/main/java/study/TestHttpServerHandler.java
@@ -17,6 +17,7 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
 
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+		System.out.println(msg.getClass().toString());
 		if (msg instanceof HttpRequest) {
 			ByteBuf content = Unpooled.copiedBuffer("hello world", CharsetUtil.UTF_8);
 			FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK,
@@ -24,7 +25,45 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
 			response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
 			response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
 			ctx.writeAndFlush(response);
+
+			// ctx.channel().close()
 		}
+	}
+
+	@Override
+	public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("channelRegistered");
+		super.channelRegistered(ctx);
+	}
+
+	@Override
+	public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("channelUnregistered");
+		super.channelUnregistered(ctx);
+	}
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("channelActive");
+		super.channelActive(ctx);
+	}
+
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("channelInactive");
+		super.channelInactive(ctx);
+	}
+
+	@Override
+	public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("handlerAdded");
+		super.handlerAdded(ctx);
+	}
+
+	@Override
+	public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("handlerRemoved");
+		super.handlerRemoved(ctx);
 	}
 
 }
