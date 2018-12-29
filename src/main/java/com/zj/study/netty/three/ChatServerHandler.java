9ae70d1f74f1d0aca15eb4b2a411c788e2fa594d
zhaojian770627 <zhaojian770627@163.com>
2018-12-29 21:40:40 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- src/main/java/com/zj/study/netty/three/ChatServerHandler.java
+++ src/main/java/com/zj/study/netty/three/ChatServerHandler.java
@@ -1,7 +1,5 @@
 package com.zj.study.netty.three;
 
-import java.util.UUID;
-
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
@@ -9,8 +7,8 @@ public class ChatServerHandler extends SimpleChannelInboundHandler<String> {
 
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
-		System.out.println(ctx.channel().remoteAddress() + " " + msg);
-		ctx.channel().writeAndFlush("from server:" + UUID.randomUUID().toString());
+
+		
 	}
 
 	@Override
