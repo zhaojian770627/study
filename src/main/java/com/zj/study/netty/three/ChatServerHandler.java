b7e150258977f39c286d746aed3b206fad356fa5
zhaojian770627 <zhaojian770627@163.com>
2018-12-31 21:06:47 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- src/main/java/com/zj/study/netty/three/ChatServerHandler.java
+++ src/main/java/com/zj/study/netty/three/ChatServerHandler.java
@@ -1,20 +1,58 @@
 package com.zj.study.netty.three;
 
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
 
 public class ChatServerHandler extends SimpleChannelInboundHandler<String> {
 
+	private static ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+		Channel channel = ctx.channel();
 
-		
+		for (Channel ch : channelGroup) {
+			if (channel != ch) {
+				ch.writeAndFlush(channel.remoteAddress() + " 发送的消息:" + msg + "\n");
+			} else {
+				ch.writeAndFlush("[self]" + msg + "\n");
+			}
+		}
 	}
 
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
 		cause.printStackTrace();
 		ctx.close();
+	}
+
+	@Override
+	public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+		Channel channel = ctx.channel();
+		channelGroup.writeAndFlush("[server]-" + channel.remoteAddress() + " added\n");
+		channelGroup.add(channel);
+	}
+
+	@Override
+	public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+		Channel channel = ctx.channel();
+		channelGroup.writeAndFlush("[server]-" + channel.remoteAddress() + " leaved\n");
+	}
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		Channel channel = ctx.channel();
+		System.out.println(channel.remoteAddress() + " 上线");
+	}
+
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		Channel channel = ctx.channel();
+		System.out.println(channel.remoteAddress() + " 下线");
 	}
 
 }
