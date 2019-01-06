f3e9b65519ed33293a1baf7618245309989f04da
zhaojian770627 <zhaojian770627@163.com>
2019-01-06 17:30:54 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- src/main/java/com/zj/study/netty/five/WebSocketServerHandler.java
+++ src/main/java/com/zj/study/netty/five/WebSocketServerHandler.java
@@ -1,14 +1,33 @@
 package com.zj.study.netty.five;
 
+import java.time.LocalDateTime;
+
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 
-public class WebSocketServerHandler extends SimpleChannelInboundHandler<WebSocketFrame> {
+public class WebSocketServerHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
 
 	@Override
-	protected void channelRead0(ChannelHandlerContext ctx, WebSocketFrame msg) throws Exception {
-
+	protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+		System.out.println("received msg:" + msg.text());
+		ctx.writeAndFlush(new TextWebSocketFrame("server time: " + LocalDateTime.now()));
 	}
 
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		System.out.println("exception happend");
+		cause.printStackTrace();
+		ctx.close();
+	}
+
+	@Override
+	public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("handlerAdded:" + ctx.channel().id().asLongText());
+	}
+
+	@Override
+	public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("handlerRemoved:" + ctx.channel().id().asLongText());
+	}
 }
