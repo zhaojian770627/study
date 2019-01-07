4c4dfcd75b7a6dc513024c5b06eb517d21f78727
zhaojianc <zhaojianc@yonyou.com>
2019-01-07 13:46:55 +0800

    add
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

---
+++ src/main/java/com/zj/study/nio/t1.java
@@ -0,0 +1,32 @@
+package com.zj.study.nio;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class t1 {
+
+	public static void main(String[] args) throws IOException {
+		RandomAccessFile aFile = new RandomAccessFile("nio-data.txt", "rw");
+		FileChannel inChannel = aFile.getChannel();
+
+		ByteBuffer buf = ByteBuffer.allocate(48);
+
+		int bytesRead = inChannel.read(buf);
+		while (bytesRead != -1) {
+
+			System.out.println("Read " + bytesRead);
+			buf.flip();
+
+			while (buf.hasRemaining()) {
+				System.out.print((char) buf.get());
+			}
+
+			buf.clear();
+			bytesRead = inChannel.read(buf);
+		}
+		aFile.close();
+	}
+
+}
