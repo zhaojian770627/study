28da0efd0398923926136e94e86ef8edad96b6aa
zhaojian770627 <zhaojian770627@163.com>
2019-01-11 22:33:25 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

---
+++ src/main/java/com/zj/study/protobuf/ProtoBufTest.java
@@ -0,0 +1,14 @@
+package com.zj.study.protobuf;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+public class ProtoBufTest {
+
+	public static void main(String[] args) throws InvalidProtocolBufferException {
+		DataInfo.Student s1 = DataInfo.Student.newBuilder().setName("赵建").setAge(20).setAddress("beijing").build();
+		byte[] student2ByteArray = s1.toByteArray();
+		DataInfo.Student s2 = DataInfo.Student.parseFrom(student2ByteArray);
+		System.out.println(s2);
+	}
+
+}
