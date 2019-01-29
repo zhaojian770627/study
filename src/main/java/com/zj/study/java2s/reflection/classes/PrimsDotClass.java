c870bfeb18cf597d909d6b7adda83611c0598325
zhaojian770627 <zhaojian770627@163.com>
2019-01-29 21:26:42 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

---
+++ src/main/java/com/zj/study/java2s/reflection/classes/PrimsDotClass.java
@@ -0,0 +1,16 @@
+package com.zj.study.java2s.reflection.classes;
+
+import java.lang.reflect.Method;
+
+/**
+ * Show that you can, in fact, take the .class of a primitive.
+ */
+public class PrimsDotClass {
+
+	public static void main(String[] args) {
+		Class c = int.class;
+		System.out.println(c.getName());
+		Method[] methods = c.getMethods();
+		System.out.println(c.getName() + " has " + methods.length + " methods");
+	}
+}
