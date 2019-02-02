72d404bd2f8aefc98d83d7389b41f3a5c3221a4a
zhaojian770627 <zhaojian770627@163.com>
2019-02-02 09:35:12 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

---
+++ src/main/java/com/zj/study/java2s/reflection/classloader/TestLoader.java
@@ -0,0 +1,22 @@
+package com.zj.study.java2s.reflection.classloader;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+
+public class TestLoader {
+
+	public static void main(String[] args) throws Throwable {
+		File file = new File("/home/zj/mlj.jar");
+		URL url = file.toURI().toURL();
+		JarClassLoader loader = new JarClassLoader(url);
+		Thread.currentThread().setContextClassLoader(loader);
+		// ClassUtils util=new ClassUtils();
+		// Class c = ClassUtils.forName("ai.Eight.Main");
+		Class c = Class.forName("ai.Eight.Main", false, loader);
+		Method m = c.getMethod("main", String[].class);
+		// m.invoke(null, (Object) new String[] {});
+		ClassUtils.invokeStaticMethod(c, "main", (Object) new String[] {}, String[].class);
+	}
+
+}
