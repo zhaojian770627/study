5fba1c769913d2e2c992452f3482e3a7ef2afe9b
zhaojian770627 <zhaojian770627@163.com>
2019-02-10 12:53:37 +0800

    aaa
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

---
+++ src/main/java/com/zj/study/java2s/reflection/classpath/TestClassPath.java
@@ -0,0 +1,8 @@
+package com.zj.study.java2s.reflection.classpath;
+
+public class TestClassPath {
+
+	public static void main(String[] args) {
+		System.out.println(ClassPath.getClassPath());
+	}
+}
