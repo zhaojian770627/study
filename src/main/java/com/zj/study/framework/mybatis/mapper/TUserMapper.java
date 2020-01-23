68de3304a0ae199f1aa086ca42c85fbaf0a547b1
zhaojian770627 <zhaojian770627@163.com>
2020-01-23 22:31:40 +0800

    mybatis
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

---
+++ src/main/java/com/zj/study/framework/mybatis/mapper/TUserMapper.java
@@ -0,0 +1,9 @@
+package com.zj.study.framework.mybatis.mapper;
+
+import com.zj.study.framework.mybatis.entity.TUser;
+
+public interface TUserMapper {
+	
+    TUser selectByPrimaryKey(Integer id);
+    
+}
