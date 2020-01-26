532ca9cfcdf7f75e0cf01bcecc35daeed49fc532
zhaojian770627 <zhaojian770627@163.com>
2020-01-26 09:39:00 +0800

    mybatis
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- src/main/java/com/zj/study/framework/mybatis/entity/TUser.java
+++ src/main/java/com/zj/study/framework/mybatis/entity/TUser.java
@@ -1,100 +1,96 @@
 package com.zj.study.framework.mybatis.entity;
 
-
 public class TUser {
-    private Integer id;
+	private Integer id;
 
-    private String userName;
+	private String userName;
 
-    private String realName;
+	private String realName;
 
-    private Byte sex;
+	private Byte sex;
 
-    private String mobile;
+	private String mobile;
 
-    private String email;
+	private String email;
 
-    private String note;
+	private String note;
 
-    private Integer positionId;
-    
+	private Integer positionId;
 
-    
-
+	public TUser(Integer id, String userName) {
+		super();
+		this.id = id;
+		this.userName = userName;
+	}
 
 	public Integer getId() {
-        return id;
-    }
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public String getUserName() {
-        return userName;
-    }
+	public String getUserName() {
+		return userName;
+	}
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
-    public String getRealName() {
-        return realName;
-    }
+	public String getRealName() {
+		return realName;
+	}
 
-    public void setRealName(String realName) {
-        this.realName = realName;
-    }
+	public void setRealName(String realName) {
+		this.realName = realName;
+	}
 
-    public Byte getSex() {
-        return sex;
-    }
+	public Byte getSex() {
+		return sex;
+	}
 
-    public void setSex(Byte sex) {
-        this.sex = sex;
-    }
+	public void setSex(Byte sex) {
+		this.sex = sex;
+	}
 
-    public String getMobile() {
-        return mobile;
-    }
+	public String getMobile() {
+		return mobile;
+	}
 
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getNote() {
-        return note;
-    }
+	public String getNote() {
+		return note;
+	}
 
-    public void setNote(String note) {
-        this.note = note;
-    }
+	public void setNote(String note) {
+		this.note = note;
+	}
 
-    public Integer getPositionId() {
-        return positionId;
-    }
+	public Integer getPositionId() {
+		return positionId;
+	}
 
-    public void setPositionId(Integer positionId) {
-        this.positionId = positionId;
-    }
-    
+	public void setPositionId(Integer positionId) {
+		this.positionId = positionId;
+	}
 
 	@Override
 	public String toString() {
-		return "TUser [id=" + id + ", userName=" + userName + ", realName="
-				+ realName + ", sex=" + sex + ", mobile=" + mobile + ", email="
-				+ email + ", note=" + note + ", positionId=" + positionId + "]";
+		return "TUser [id=" + id + ", userName=" + userName + ", realName=" + realName + ", sex=" + sex + ", mobile="
+				+ mobile + ", email=" + email + ", note=" + note + ", positionId=" + positionId + "]";
 	}
 
-	
-	
-    
 }
