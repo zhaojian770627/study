e38c5090518e12c1f34cda1a0658f7ca163ba6a4
zhaojian770627 <zhaojian770627@163.com>
2020-01-27 11:13:13 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- src/main/java/com/zj/study/framework/mybatis/MybatisQuickStart.java
+++ src/main/java/com/zj/study/framework/mybatis/MybatisQuickStart.java
@@ -2,6 +2,7 @@ package com.zj.study.framework.mybatis;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -38,8 +39,7 @@ public class MybatisQuickStart {
 		System.out.println(user.toString());
 
 	}
-	
-	
+
 	@Test
 	// 测试插入数据自动生成id
 	public void testInsertGenerateId1() throws IOException {
@@ -52,11 +52,44 @@ public class MybatisQuickStart {
 		user1.setUserName("test1");
 		user1.setRealName("realname1");
 		user1.setEmail("myemail1");
-//		user1.setId(34);
+		// user1.setId(34);
 		mapper.insert1(user1);
 		sqlSession.commit();
 		System.out.println(user1.getId());
 	}
 
+	@Test
+	// 测试插入数据自动生成id
+	public void testInsertGenerateId2() throws IOException {
+		// 2.获取sqlSession
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		// 3.获取对应mapper
+		TUserMapper mapper = sqlSession.getMapper(TUserMapper.class);
+		// 4.执行查询语句并返回结果
+		TUser user2 = new TUser();
+		user2.setUserName("test2");
+		user2.setRealName("realname2");
+		user2.setEmail("myemai2l");
+		mapper.insert2(user2);
+		sqlSession.commit();
+		System.out.println(user2.getId());
+	}
 
+	@Test
+	// 参数#和参数$区别测试(动态sql 入门)
+	public void testSymbol() {
+		// 2.获取sqlSession
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		// 3.获取对应mapper
+		TUserMapper mapper = sqlSession.getMapper(TUserMapper.class);
+
+		String inCol = "id, user_name, real_name, sex, mobile, email, note";
+		String tableName = "t_user";
+		Byte sex = 1;
+		String orderStr = "sex,user_name";
+
+		List<TUser> list = mapper.selectBySymbol(tableName, inCol, orderStr, sex);
+		System.out.println(list.size());
+
+	}
 }
--- src/main/java/com/zj/study/framework/mybatis/mapper/TUserMapper.java
+++ src/main/java/com/zj/study/framework/mybatis/mapper/TUserMapper.java
@@ -1,10 +1,19 @@
 package com.zj.study.framework.mybatis.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.zj.study.framework.mybatis.entity.TUser;
 
 public interface TUserMapper {
-	
-    TUser selectByPrimaryKey(Integer id);
-    
-    int insert1(TUser record);
+
+	TUser selectByPrimaryKey(Integer id);
+
+	int insert1(TUser record);
+
+	int insert2(TUser record);
+
+	List<TUser> selectBySymbol(@Param("tableName") String tableName, @Param("inCol") String inCol,
+			@Param("orderStr") String orderStr, @Param("sex") Byte sex);
 }
