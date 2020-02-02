9358446b9775ea756a8e6d17ca9917c381070310
zhaojian770627 <zhaojian770627@163.com>
2020-02-02 15:46:25 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- src/main/java/com/zj/study/framework/mybatis/spring/service/UserService.java
+++ src/main/java/com/zj/study/framework/mybatis/spring/service/UserService.java
@@ -3,6 +3,7 @@ package com.zj.study.framework.mybatis.spring.service;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.zj.study.framework.mybatis.entity.TUser;
 import com.zj.study.framework.mybatis.mapper.TUserMapper;
@@ -15,7 +16,8 @@ public class UserService {
 	public TUser getUserById(int id) {
 		return userMapper.selectByPrimaryKey(id);
 	}
-	
+
+	@Transactional
 	public void addUser(TUser user) {
 		userMapper.insert1(user);
 	}
