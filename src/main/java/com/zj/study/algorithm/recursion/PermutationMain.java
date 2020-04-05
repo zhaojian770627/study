631c13c9c77fae4e94b65f5a6a0bf3b8f82ee525
zhaojian770627 <zhaojian770627@163.com>
2020-04-05 13:27:16 +0800

    up
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- src/main/java/com/zj/study/algorithm/recursion/PermutationMain.java
+++ src/main/java/com/zj/study/algorithm/recursion/PermutationMain.java
@@ -1,13 +1,9 @@
 package com.zj.study.algorithm.recursion;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class PermutationMain {
 
 	public static void main(String[] args) {
-		List<String[]> result = new ArrayList<>();
-		permutation("", "abc");
+		permutationUnique("", "223");
 //		permutation(result, new String[] { "a", "b", "c" }, 0);
 //		System.out.print("(");
 //		for (String[] str : result) {
@@ -25,6 +21,18 @@ public class PermutationMain {
 			System.out.println(result);
 		for (int i = 0; i < input.length(); i++) {
 			permutation(result + input.substring(i, i + 1), input.substring(0, i) + input.substring(i + 1));
+		}
+	}
+
+	private static void permutationUnique(String result, String input) {
+		if (input.length() == 0) {
+			System.out.println(result);
+		}
+		for (int i = 0; i < input.length(); i++) {
+			if (i != 0 && input.substring(i, i + 1).equals(input.substring(i - 1, i))) {
+				continue;
+			}
+			permutationUnique(result + input.substring(i, i + 1), input.substring(0, i) + input.substring(i + 1));
 		}
 	}
 }
