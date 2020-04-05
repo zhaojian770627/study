1076f21b8960140c22fb3e593bb0929bec74b554
zhaojian770627 <zhaojian770627@163.com>
2020-04-05 16:00:40 +0800

    aaa
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

---
+++ src/main/java/com/zj/study/algorithm/recursion/CombSumMain.java
@@ -0,0 +1,48 @@
+package com.zj.study.algorithm.recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class CombSumMain {
+
+	public static void main(String[] args) {
+		CombSumMain main = new CombSumMain();
+		// main.subsets(3);
+		main.combSum(new int[] { 1, 2, 3 }, 7);
+	}
+
+	private void combSum(int[] ary, int sum) {
+		Stack<Integer> stack = new Stack<>();
+		List<Integer[]> result = new ArrayList<>();
+		subsets_recursive_helper(result, stack, ary, sum, 0);
+
+		System.out.print("[");
+		for (Integer[] ss : result) {
+			System.out.print("(");
+			for (int s : ss) {
+				System.out.print(s + " ");
+			}
+			System.out.print(") ");
+		}
+		System.out.print("]");
+	}
+
+	// [() (a ) (a b ) (a b c ) (a c ) (b ) (b c ) (c ) ]
+	// 每次从递归中退出,函数全部pop,而上一层还停留指定的位置
+	private void subsets_recursive_helper(List<Integer[]> result, Stack<Integer> queue, int[] ary, int remains, int i) {
+		if (remains < 0)
+			return;
+		if (remains == 0) {
+			result.add(queue.toArray(new Integer[] {}));
+		} else
+			for (int j = i; j < ary.length; j++) {
+				queue.push(ary[j]);
+				subsets_recursive_helper(result, queue, ary, remains - ary[j], j + 1);
+				queue.pop();
+			}
+	}
+
+}
