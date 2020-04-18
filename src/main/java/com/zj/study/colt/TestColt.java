3b5abd21c966c38aec2c1e1a6d28229db9466976
zhaojianc <zhaojian770627@163.com>
2020-04-18 10:35:53 +0800

    up
    Signed-off-by: zhaojianc <zhaojian770627@163.com>

--- src/main/java/com/zj/study/colt/TestColt.java
+++ src/main/java/com/zj/study/colt/TestColt.java
@@ -1,5 +1,6 @@
 package com.zj.study.colt;
 
+import java.io.IOException;
 import java.util.Random;
 
 import cern.colt.matrix.DoubleMatrix2D;
@@ -8,18 +9,19 @@ import cern.colt.matrix.linalg.Algebra;
 
 /**
  * 
- * 耗费时间:383797
- *	耗费内存:3808k
+ * 耗费时间:383797 耗费内存:3808k
  * 
  * @author zhaojianc
  *
  */
 public class TestColt {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Runtime runtime = Runtime.getRuntime();
 		long startMem = runtime.freeMemory();
 		int size = 10000;
+		System.out.println("Please enter a key to compute:");
+		System.in.read();
 		Random r = new Random();
 		// 生成随机的系数矩阵100个
 		DenseDoubleMatrix2D matrix = new DenseDoubleMatrix2D(size, size);
@@ -40,13 +42,17 @@ public class TestColt {
 		// 矩阵相乘,得出结果矩阵
 		Algebra algebra = new Algebra();
 		DoubleMatrix2D D = algebra.mult(matrix, x);
-
+		// help GC
+		x = null;
+		System.out.println("Start to Compute...");
 		long start = System.currentTimeMillis();
 		DoubleMatrix2D xr = algebra.solve(matrix, D);
 		long end = System.currentTimeMillis();
 		long orz = startMem - runtime.freeMemory();
 		System.out.println("耗费时间:" + (end - start));
 		System.out.println("耗费内存:" + orz / 1024 + "k");
+		System.out.println("Please enter a key to end:");
+		System.in.read();
 	}
 
 }
