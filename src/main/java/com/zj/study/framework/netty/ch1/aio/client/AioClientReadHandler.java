3fe34e8f7b300b05ca2ce0a8d33618c484a8840a
zhaojianc <zhaojian770627@163.com>
2020-04-18 11:28:54 +0800

    up
    Signed-off-by: zhaojianc <zhaojian770627@163.com>

--- src/main/java/com/zj/study/framework/netty/ch1/aio/client/AioClientReadHandler.java
+++ src/main/java/com/zj/study/framework/netty/ch1/aio/client/AioClientReadHandler.java
@@ -1,25 +1,43 @@
 package com.zj.study.framework.netty.ch1.aio.client;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
 import java.util.concurrent.CountDownLatch;
 
-public class AioClientReadHandler implements CompletionHandler {
+public class AioClientReadHandler implements CompletionHandler<Integer, ByteBuffer> {
+	private AsynchronousSocketChannel clientChannel;
+	private CountDownLatch latch;
 
 	public AioClientReadHandler(AsynchronousSocketChannel clientChannel, CountDownLatch latch) {
-		// TODO Auto-generated constructor stub
+		this.clientChannel = clientChannel;
+		this.latch = latch;
 	}
 
 	@Override
-	public void completed(Object result, Object attachment) {
-		// TODO Auto-generated method stub
-
+	public void completed(Integer result, ByteBuffer buffer) {
+		buffer.flip();
+		byte[] bytes = new byte[buffer.remaining()];
+		buffer.get(bytes);
+		String msg;
+		try {
+			msg = new String(bytes, "UTF-8");
+			System.out.println("accept message:" + msg);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public void failed(Throwable exc, Object attachment) {
-		// TODO Auto-generated method stub
-
+	public void failed(Throwable exc, ByteBuffer attachment) {
+		System.err.println("数据读取失败...");
+		try {
+			clientChannel.close();
+			latch.countDown();
+		} catch (IOException e) {
+		}
 	}
 
 }
