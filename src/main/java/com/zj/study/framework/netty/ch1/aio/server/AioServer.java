6d0fa5e211898a1d226043c435bccbff125dd9fc
zhaojian770627 <zhaojian770627@163.com>
2020-04-18 16:36:53 +0800

    up
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- src/main/java/com/zj/study/framework/netty/ch1/aio/server/AioServer.java
+++ src/main/java/com/zj/study/framework/netty/ch1/aio/server/AioServer.java
@@ -1,5 +1,7 @@
 package com.zj.study.framework.netty.ch1.aio.server;
 
+import java.io.IOException;
+
 import com.zj.study.framework.netty.ch1.Ch01Const;
 
 public class AioServer {
@@ -7,14 +9,14 @@ public class AioServer {
 	// 统计客户端个数
 	public volatile static long clientCount = 0;
 
-	private static void start() {
+	private static void start() throws IOException {
 		if (serverHandle != null)
 			return;
 		serverHandle = new AioServerHandler(Ch01Const.DEFAULT_PORT);
 		new Thread(serverHandle, "Server").start();
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		AioServer.start();
 	}
 
