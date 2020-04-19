525539733197c989e4988831b2c046d3a2fdd3d8
zhaojian770627 <zhaojian770627@163.com>
2020-04-19 22:04:25 +0800

    up
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- src/main/java/com/zj/study/algorithm/sort/SortMain.java
+++ src/main/java/com/zj/study/algorithm/sort/SortMain.java
@@ -4,9 +4,10 @@ public class SortMain {
 
 	public static void main(String[] args) {
 		int[] a = new int[] { 1, 3, 5, 7, 9, 2, 4, 6, 8, 99 };
+//		int[] a = new int[] { 3, 1, 4 };
 		SortMain main = new SortMain();
-		main.selectSort(a);
-
+//		main.selectSort(a);
+		main.inserSort(a);
 		print(a);
 	}
 
@@ -16,6 +17,11 @@ public class SortMain {
 		}
 	}
 
+	/**
+	 * 选择排序
+	 * 
+	 * @param a
+	 */
 	private void selectSort(int[] a) {
 		for (int i = 0; i < a.length; i++) {
 			int min = i;
@@ -29,5 +35,39 @@ public class SortMain {
 			a[i] = a[min];
 			a[min] = t;
 		}
+	}
+
+	/**
+	 * 
+	 * 插入排序
+	 * 
+	 * @param a
+	 */
+	void inserSort(int[] a) {
+		// 两层巡皇
+		for (int i = 0; i < a.length; i++) {
+			int j = i;
+			for (; j > 0; j--) {
+				if (a[i] < a[j - 1])
+					continue;
+				else
+					break;
+			}
+
+			int t = a[i];
+			for (int k = i; k > j; k--) {
+				a[k] = a[k - 1];
+			}
+			a[j] = t;
+		}
+	}
+
+	/**
+	 * 希尔排序
+	 * 
+	 * @param a
+	 */
+	void shellSort(int[] a) {
+
 	}
 }
