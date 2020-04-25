2ef9fbfbc78e55619354503d008a492b1da97eec
zhaojianc <zhaojian770627@163.com>
2020-04-25 16:20:49 +0800

    粘包和半包
    Signed-off-by: zhaojianc <zhaojian770627@163.com>

---
+++ src/main/java/com/zj/study/framework/netty/ch2/demo/EchoClient.java
@@ -0,0 +1,45 @@
+package com.zj.study.framework.netty.ch2.demo;
+
+import java.net.InetSocketAddress;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * 
+ * 演示粘包和半包
+ * 
+ * @author zhaojianc
+ *
+ */
+public class EchoClient {
+	private final int port;
+	private final String host;
+
+	private void start() throws InterruptedException {
+		EventLoopGroup group = new NioEventLoopGroup();/* 线程组 */
+		try {
+			Bootstrap b = new Bootstrap();/* 客户端启动必备 */
+			b.group(group).channel(NioSocketChannel.class)/* 指明使用NIO进行网络通讯 */
+					.remoteAddress(new InetSocketAddress(host, port))/* 配置远程服务器的地址 */
+					.handler(new EchoClientHandler());
+			ChannelFuture f = b.connect().sync();/* 连接到远程节点，阻塞等待直到连接完成 */
+			/* 阻塞，直到channel关闭 */
+			f.channel().closeFuture().sync();
+		} finally {
+			group.shutdownGracefully().sync();
+		}
+	}
+
+	public EchoClient(int port, String host) {
+		this.port = port;
+		this.host = host;
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		new EchoClient(9999, "127.0.0.1").start();
+	}
+}
