c23bb8eb962a2e6ab7a765b4985f0cf85c5c7167
zhaojian770627 <zhaojian770627@163.com>
2020-05-24 22:06:01 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- src/main/java/com/zj/study/algorithm/search/MetricSearchMain.java
+++ src/main/java/com/zj/study/algorithm/search/MetricSearchMain.java
@@ -1,14 +1,13 @@
 package com.zj.study.algorithm.search;
 
-import cern.colt.Arrays;
-
 public class MetricSearchMain {
 
 	public static void main(String[] args) {
 		int[][] m = new int[][] { { 1, 3, 5, 7, 9 }, { 2, 4, 8, 11, 22 }, { 6, 15, 21, 24, 25 },
 				{ 13, 18, 26, 28, 30 } };
 		MetricSearchMain main = new MetricSearchMain();
-		System.out.println(Arrays.toString(main.searchOne(m, 55)));
+//		System.out.println(Arrays.toString(main.searchOne(m, 55)));
+		System.out.println(main.kthSmallest(m, 10));
 	}
 
 	/**
@@ -39,4 +38,40 @@ public class MetricSearchMain {
 
 		return new int[] { -1, -1 };
 	}
+
+	private int kthSmallest(int[][] m, int k) {
+		int row = m.length;
+		int col = m[0].length;
+
+		int L = m[0][0];
+		int R = m[row - 1][col - 1];
+		while (L < R) {
+			int mid = L + (R - L) / 2;
+			int temp = search_lower_than_mid(m, row, col, mid);
+			if (temp < k)
+				L = mid + 1;
+			else
+				R = mid;
+		}
+		return L;
+	}
+
+	private int search_lower_than_mid(int[][] m, int row, int col, int mid) {
+		// 预先定位到 row行，0列，为起点
+		int i = row - 1;
+		int j = 0;
+
+		int cnt = 0;
+
+		while (i >= 0 && j < col) {
+			if (m[i][j] <= mid) {
+				j++;
+				cnt += i + 1; // 默认第
+			} else
+				i--;
+		}
+		return cnt;
+
+	}
+
 }
