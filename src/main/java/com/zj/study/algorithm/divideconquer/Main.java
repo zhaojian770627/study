6a050358b8ba0932a6f24ff5bc4ae170bf4ba01f
zhaojianc <zhaojian770627@163.com>
2020-06-25 16:21:22 +0800

    up
    Signed-off-by: zhaojianc <zhaojian770627@163.com>

---
+++ src/main/java/com/zj/study/algorithm/divideconquer/Main.java
@@ -0,0 +1,21 @@
+package com.zj.study.algorithm.divideconquer;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+	/**
+	 * Given an array of 2n elements in the following format { a1, a2, a3, a4, …..,
+	 * an, b1, b2, b3, b4, …., bn }. The task is shuffle the array to {a1, b1, a2,
+	 * b2, a3, b3, ……, an, bn } without using extra space
+	 * 
+	 */
+	void shuffleArray(int[] a, int left, int right) {
+		if (right - left == 1)
+			return;
+		
+		
+	}
+}
