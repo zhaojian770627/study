366a7e377b57cf08bfc2d4499ac362790f36eab5
zhaojianc <zhaojian770627@163.com>
2020-06-25 16:37:48 +0800

    up
    Signed-off-by: zhaojianc <zhaojian770627@163.com>

--- src/main/java/com/zj/study/algorithm/divideconquer/Main.java
+++ src/main/java/com/zj/study/algorithm/divideconquer/Main.java
@@ -13,9 +13,31 @@ public class Main {
 	 * 
 	 */
 	void shuffleArray(int[] a, int left, int right) {
+		// If only 2 element, return
 		if (right - left == 1)
 			return;
-		
-		
+
+		// Finding mid to divide the array
+		int mid = (left + right) / 2;
+
+		// Using temp for swapping first
+		// half of second array
+		int temp = mid + 1;
+
+		// Mid is use for swapping second
+		// half for first array
+		int mmid = (left + mid) / 2;
+
+		// Swapping the element
+		for (int i = mmid + 1; i <= mid; i++) {
+			int tv = a[i];
+			a[i] = a[temp];
+			a[temp] = tv;
+		}
+
+		// Recursively doing for
+		// first half and second half
+		shuffleArray(a, left, mid);
+		shuffleArray(a, mid, right);
 	}
 }
