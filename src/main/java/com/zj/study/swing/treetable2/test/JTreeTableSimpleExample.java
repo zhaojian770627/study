732b567a7ce78ca959870b0db00bbb1ea008a2ce
zhaojianc <zhaojian770627@163.com>
2020-06-30 19:18:51 +0800

    jtreetable
    Signed-off-by: zhaojianc <zhaojian770627@163.com>

---
+++ src/main/java/com/zj/study/swing/treetable2/test/JTreeTableSimpleExample.java
@@ -0,0 +1,41 @@
+/*
+ * www.javagl.de - JTreeTable
+ *
+ * Copyright (c) 2016 Marco Hutter - http://www.javagl.de
+ */
+package com.zj.study.swing.treetable2.test;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
+
+import com.zj.study.swing.treetable2.JTreeTable;
+import com.zj.study.swing.treetable2.TreeTableModel;
+
+/**
+ * A simple example application for the JTreeTable
+ */
+@SuppressWarnings("javadoc")
+public class JTreeTableSimpleExample {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+
+	private static void createAndShowGUI() {
+		JFrame f = new JFrame();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		TreeTableModel treeTableModel = ExampleTreeTableModels.createSimple();
+		JTreeTable treeTable = new JTreeTable(treeTableModel);
+		f.getContentPane().add(new JScrollPane(treeTable));
+
+		f.setSize(400, 400);
+		f.setLocationRelativeTo(null);
+		f.setVisible(true);
+	}
+}
