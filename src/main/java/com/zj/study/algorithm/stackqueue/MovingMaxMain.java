69945dc26a23f3f853807674373d0c999cb555ff
zhaojian770627 <zhaojian770627@163.com>
2020-07-19 16:31:47 +0800

    up
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- src/main/java/com/zj/study/algorithm/stackqueue/MovingMaxMain.java
+++ src/main/java/com/zj/study/algorithm/stackqueue/MovingMaxMain.java
@@ -2,6 +2,7 @@ package com.zj.study.algorithm.stackqueue;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Stack;
 
 /**
  * 滑动窗口取最大值
@@ -15,31 +16,44 @@ public class MovingMaxMain {
 		int[] ary = { 12, 1, 78, 90, 57, 89, 56 };
 
 		main.movingMax(ary, 3);
+
+//		main.testDeque();
+	}
+
+	private void testDeque() {
+		Stack<Integer> qi = new Stack<>();
+		qi.push(0);
+		qi.push(1);
+
+//		qi.pop();
+
+		System.out.println(qi.firstElement());
 	}
 
 	private void movingMax(int[] ary, int k) {
 		int n = ary.length;
-		Deque<Integer> qi = new ArrayDeque<>();
+
+		Stack<Integer> qi = new Stack<>();
 
 		for (int i = 0; i < k; i++) {
 			while (qi.size() > 0 && ary[i] >= ary[qi.peek()])
 				qi.pop();
 
-			qi.add(i);
+			qi.push(i);
 		}
 
 		for (int i = k; i < n; i++) {
-			System.out.println(ary[qi.peek()]);
+			System.out.println(ary[qi.firstElement()]);
 
-			while (qi.size() > 0 && qi.peek() <= i - k)
-				qi.pop();
+			while (qi.size() > 0 && qi.get(0) <= i - k)
+				qi.remove(0);
 
 			while (qi.size() > 0 && ary[i] >= ary[qi.peek()])
 				qi.pop();
 
-			qi.add(i);
+			qi.push(i);
 		}
 
-		System.out.println(ary[qi.peek()]);
+		System.out.println(ary[qi.firstElement()]);
 	}
 }
---
+++ src/main/java/com/zj/study/algorithm/stackqueue/test.py
@@ -0,0 +1,9 @@
+from collections import deque
+
+qi = deque()
+qi.append(0)
+qi.append(1)
+
+qi.pop()
+
+print(qi[0])
