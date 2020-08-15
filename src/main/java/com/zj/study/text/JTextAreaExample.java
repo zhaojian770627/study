8741488c9165636aaedbfe02e49ce3399c4b691f
zhaojianc <zhaojian770627@163.com>
2020-08-15 10:03:14 +0800

    aa
    Signed-off-by: zhaojianc <zhaojian770627@163.com>

--- src/main/java/com/zj/study/text/JTextAreaExample.java
+++ src/main/java/com/zj/study/text/JTextAreaExample.java
@@ -1,11 +1,19 @@
 package com.zj.study.text;
 
+import java.awt.BorderLayout;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+
+import org.apache.jute.compiler.JBuffer;
 
 /**
  * A TreeTable example, showing a JTreeTable, operating on the local file
@@ -25,6 +33,7 @@ public class JTextAreaExample {
 		JFrame frame = new JFrame("TreeTable");
 //		MyJTextArea textArea = new MyJTextArea();
 		JTextArea textArea = new JTextArea();
+		JButton topButton = new JButton("TOP");
 
 		frame.addWindowListener(new WindowAdapter() {
 			@Override
@@ -33,9 +42,18 @@ public class JTextAreaExample {
 			}
 		});
 
-		textArea.setText("abcd\neeeee");
+		topButton.addActionListener(new ActionListener() {
 
-		frame.getContentPane().add(new JScrollPane(textArea));
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				textArea.getUI().viewToModel(textArea, new Point(10, 10));
+			}
+		});
+
+		textArea.setText("abcd\neeeee");
+		frame.getContentPane().setLayout(new BorderLayout());
+		frame.getContentPane().add(new JScrollPane(topButton), BorderLayout.NORTH);
+		frame.getContentPane().add(new JScrollPane(textArea), BorderLayout.CENTER);
 		frame.pack();
 		frame.setSize(600, 800);
 		frame.show();
