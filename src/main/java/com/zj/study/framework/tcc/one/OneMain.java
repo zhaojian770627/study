ee326a2ce79822a8b387cd0bc57e89cbe58b1211
zhaojianc <zhaojian770627@163.com>
2020-08-17 16:42:42 +0800

    up
    Signed-off-by: zhaojianc <zhaojian770627@163.com>

---
+++ src/main/java/com/zj/study/framework/tcc/one/OneMain.java
@@ -0,0 +1,15 @@
+package com.zj.study.framework.tcc.one;
+
+import java.io.IOException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class OneMain {
+
+	public static void main(String[] args) throws IOException {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"classpath:/com/zj/study/framework/tcc/one/xml/spring_one.xml");
+	}
+
+}
