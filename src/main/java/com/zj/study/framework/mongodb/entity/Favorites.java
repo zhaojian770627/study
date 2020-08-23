9b4d01764e54cdd0cec7bef9d919c1fae436f7d5
zhaojian770627 <zhaojian770627@163.com>
2020-08-23 16:43:00 +0800

    up
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

---
+++ src/main/java/com/zj/study/framework/mongodb/entity/Favorites.java
@@ -0,0 +1,28 @@
+package com.zj.study.framework.mongodb.entity;
+
+import java.util.List;
+
+public class Favorites {
+	private List<String> movies;
+	private List<String> cites;
+	public List<String> getMovies() {
+		return movies;
+	}
+	public void setMovies(List<String> movies) {
+		this.movies = movies;
+	}
+	public List<String> getCites() {
+		return cites;
+	}
+	public void setCites(List<String> cites) {
+		this.cites = cites;
+	}
+	@Override
+	public String toString() {
+		return "Favorites [movies=" + movies + ", cites=" + cites + "]";
+	}
+	
+	
+	
+	
+}
