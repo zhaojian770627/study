d54c7c56b1f66406616ce8d8ff33ff814acb9825
zhaojianc <zhaojian770627@163.com>
2020-09-24 20:03:10 +0800

    加入红黑树
    Signed-off-by: zhaojianc <zhaojian770627@163.com>

---
+++ src/main/java/com/zj/study/algorithm/tree/rb/UnderflowException.java
@@ -0,0 +1,9 @@
+package com.zj.study.algorithm.tree.rb;
+/**
+ * Exception class for access in empty containers
+ * such as stacks, queues, and priority queues.
+ * @author Mark Allen Weiss
+ */
+public class UnderflowException extends RuntimeException
+{
+}
