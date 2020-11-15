53a981848890ba6e6bccde901e09b82a28d503d7
zhaojian770627 <zhaojian770627@163.com>
2020-11-15 20:08:00 +0800

    up
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- src/main/java/com/zj/study/freemarker/DBMetaDataGet.java
+++ src/main/java/com/zj/study/freemarker/DBMetaDataGet.java
@@ -2,7 +2,10 @@ package com.zj.study.freemarker;
 
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
+import java.sql.ParameterMetaData;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 /**
@@ -37,6 +40,26 @@ public class DBMetaDataGet {
 		ResultSet columns = metaData.getColumns(null, null, "product", null);
 		while (columns.next()) {
 			System.out.println(columns.getString("column_name"));
+		}
+
+		System.out.println("STATEMENT--------------------------------------------");
+		String sql = "select * from product where id=?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, 1);
+		ParameterMetaData parameterMetaData = pstmt.getParameterMetaData();
+		System.out.println(parameterMetaData.getParameterCount());
+
+		System.out.println("ResultSet--------------------------------------------");
+		ResultSet executeResult = pstmt.executeQuery();
+		ResultSetMetaData resultSetMetaData = executeResult.getMetaData();
+		int columnCount = resultSetMetaData.getColumnCount();
+		for (int i = 1; i < +columnCount; i++) {
+			String columnName = resultSetMetaData.getColumnName(i);
+			String columnTypeName = resultSetMetaData.getColumnTypeName(i);
+			String columnClassName = resultSetMetaData.getColumnClassName(i);
+
+			System.out.println("columnName:" + columnName + ",columnTypeName:" + columnTypeName + ",columnClassName:"
+					+ columnClassName);
 		}
 
 		catalogs.close();
