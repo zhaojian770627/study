0b668e1bd65bf42df707818445e4c152aa2b1763
zhaojian770627 <zhaojian770627@163.com>
2020-12-11 16:49:21 +0800

    uuu
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- src/main/java/com/zj/study/framework/spring/lock/config/LockBuilder.java
+++ src/main/java/com/zj/study/framework/spring/lock/config/LockBuilder.java
@@ -3,16 +3,12 @@ package com.zj.study.framework.spring.lock.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import com.zj.study.framework.spring.lock.mdd.redis.RedisClient;
 
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
-//配置类====配置文件
-@Configuration
 public class LockBuilder {
 
 	public static RedissonClient BuildRedissonClient(RedisConf conf) {
