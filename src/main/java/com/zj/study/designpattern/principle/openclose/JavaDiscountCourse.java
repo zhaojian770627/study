e8706c501eee663fa364159a805c73e4f4c92f07
zhaojian770627 <zhaojian770627@163.com>
2021-01-11 09:29:44 +0800

    openclose
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

---
+++ src/main/java/com/zj/study/designpattern/principle/openclose/JavaDiscountCourse.java
@@ -0,0 +1,16 @@
+package com.zj.study.designpattern.principle.openclose;
+
+/**
+ * Created by geely
+ */
+public class JavaDiscountCourse extends JavaCourse {
+
+    public JavaDiscountCourse(Integer id, String name, Double price) {
+        super(id, name, price);
+    }
+
+    public Double getDiscountPrice(){
+        return super.getPrice()*0.8;
+    }
+
+}
