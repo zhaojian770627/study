830b5672e4b8d6379b7ba214c098f178dada2aa7
zhaojian770627 <zhaojian770627@163.com>
2021-01-17 11:09:11 +0800

    up
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

--- src/main/java/com/zj/study/designpattern/pattern/creational/simplefactory/Test.java
+++ src/main/java/com/zj/study/designpattern/pattern/creational/simplefactory/Test.java
@@ -2,6 +2,12 @@ package com.zj.study.designpattern.pattern.creational.simplefactory;
 
 /**
  * Created by geely
+ * 
+ * 简单工厂实现
+ * 
+ * 相关
+ * JDK：Calendar
+ * Logback:LoggerFactory
  */
 public class Test {
     public static void main(String[] args) {
