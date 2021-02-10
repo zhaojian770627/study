159fedd641b347309cbc22a0449a043cc92e27af
zhaojian770627 <zhaojian770627@163.com>
2021-02-10 11:16:18 +0800

    add bridge pattern
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

---
+++ src/main/java/com/zj/study/designpattern/pattern/structural/bridge/Account.java
@@ -0,0 +1,10 @@
+package com.zj.study.designpattern.pattern.structural.bridge;
+
+/**
+ * Created by geely
+ */
+public interface Account {
+    Account openAccount();
+    void showAccountType();
+
+}
