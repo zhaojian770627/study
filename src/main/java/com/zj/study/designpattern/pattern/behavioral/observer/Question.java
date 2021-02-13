af78c9ff76d5ded9b909756848ed0bb0b5e2fc47
zhaojian770627 <zhaojian770627@163.com>
2021-02-13 16:40:00 +0800

    add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

---
+++ src/main/java/com/zj/study/designpattern/pattern/behavioral/observer/Question.java
@@ -0,0 +1,18 @@
+package com.zj.study.designpattern.pattern.behavioral.observer;
+
+public class Question {
+	private String userName;
+	private String questionContent;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getQuestionContent() {
+		return questionContent;
+	}
+	public void setQuestionContent(String questionContent) {
+		this.questionContent = questionContent;
+	}
+}
