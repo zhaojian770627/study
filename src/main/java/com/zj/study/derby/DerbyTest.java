f099dfadc47df05d2f3c6ddffe5c29e9514f3895
zhaojianc <zhaojianc@yonyou.com>
2021-05-11 19:46:21 +0800

    up
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- src/main/java/com/zj/study/derby/DerbyTest.java
+++ src/main/java/com/zj/study/derby/DerbyTest.java
@@ -7,24 +7,27 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.apache.derby.shared.common.sanity.SanityManager;
+
 public class DerbyTest {
 
-
 	public static void main(String[] args) throws Exception {
+		SanityManager.DEBUG_SET("ClassLineNumbers");
+		SanityManager.DEBUG_SET("DumpClassFile");
 		Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
 		Connection conn = DriverManager.getConnection("jdbc:derby:d://data//derbydb;create=true");
 //		Connection conn = DriverManager.getConnection("jdbc:derby:derbydb");
-		
+
 //		createDB(conn);
-		
+
 		PreparedStatement pstate1 = conn.prepareStatement("select * from derbytable where id = ?");
 		pstate1.setInt(1, 2);
 		ResultSet rset1 = pstate1.executeQuery();
-		while(rset1.next()) {
-			System.out.println(rset1.getInt(1)+">"+rset1.getString(2));
+		while (rset1.next()) {
+			System.out.println(rset1.getInt(1) + ">" + rset1.getString(2));
 		}
 		pstate1.close();
-		
+
 		conn.close();
 	}
 
@@ -32,7 +35,7 @@ public class DerbyTest {
 		Statement state = conn.createStatement();
 		state.executeUpdate("create table derbytable(id int,val varchar(128))");
 		state.close();
-		
+
 		Statement state2 = conn.createStatement();
 		state2.executeUpdate("insert into derbytable values (1,'tom') ");
 		state2.executeUpdate("insert into derbytable values (2,'jerry') ");
