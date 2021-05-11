6c380ae35f2320b64bbb98fa179368ca8affe2ee
zhaojianc <zhaojianc@yonyou.com>
2021-05-11 21:16:44 +0800

    up
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- src/main/java/com/zj/study/derby/BCJavaTest.java
+++ src/main/java/com/zj/study/derby/BCJavaTest.java
@@ -4,21 +4,28 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
 import org.apache.derby.iapi.services.cache.CacheManager;
+import org.apache.derby.iapi.services.compiler.ClassBuilder;
 import org.apache.derby.impl.services.bytecode.BCJava;
 import org.apache.derby.impl.services.cache.ConcurrentCacheFactory;
 import org.apache.derby.impl.services.reflect.ReflectClassesJava2;
+import org.apache.derby.shared.common.sanity.SanityManager;
 
 public class BCJavaTest {
 
 	public static void main(String[] args) throws Exception {
+		SanityManager.DEBUG_SET("ClassLineNumbers");
+		SanityManager.DEBUG_SET("DumpClassFile");
+		SanityManager.DEBUG_SET("ByteCodeGenInstr");
+
 		BCJava bcJava = new BCJava();
 
 		setCache(bcJava);
 
 		int modifiers = Modifier.PUBLIC | Modifier.FINAL;
 
-		bcJava.newClassBuilder(new ReflectClassesJava2(), "a.b.c", modifiers, "ddd", null);
-		System.err.println("Run success!");
+		ClassBuilder bcClass = bcJava.newClassBuilder(new ReflectClassesJava2(), "a.b.c", modifiers, "ddd", null);
+		bcClass.getClassBytecode();
+		System.err.println(bcClass);
 	}
 
 	private static void setCache(BCJava bcJava) throws Exception {
