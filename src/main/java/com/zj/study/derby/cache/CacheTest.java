edf9fa152bf4c25fa4fa52dd58561154ce7aa347
zhaojianc <zhaojianc@yonyou.com>
2021-05-15 17:13:37 +0800

    up
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- src/main/java/com/zj/study/derby/cache/CacheTest.java
+++ src/main/java/com/zj/study/derby/cache/CacheTest.java
@@ -1,13 +1,17 @@
 package com.zj.study.derby.cache;
 
+import org.apache.derby.iapi.error.StandardException;
 import org.apache.derby.iapi.services.cache.CacheManager;
+import org.apache.derby.iapi.services.cache.Cacheable;
 import org.apache.derby.impl.services.cache.ConcurrentCacheFactory;
 
 public class CacheTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws StandardException {
 		ConcurrentCacheFactory cacheFactory = new ConcurrentCacheFactory();
 		CacheManager cm = cacheFactory.newCacheManager(new MyObjectFactory(), "MyObject", 64, 256);
+		Cacheable myObject = cm.find("A");
+
 	}
 
 }
