fd2e3b0fa7cb684d09120aa51a436e72c2175caf
zhaojianc <zhaojianc@yonyou.com>
2021-06-09 14:27:03 +0800

    bit set test
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

---
+++ src/main/java/com/zj/study/derby/bit/BitSetTest.java
@@ -0,0 +1,9 @@
+package com.zj.study.derby.bit;
+
+import org.apache.derby.iapi.services.io.FormatableBitSet;
+
+public class BitSetTest {
+	public static void main(String[] args) {
+		FormatableBitSet.maxBitsForSpace(1281);
+	}
+}
