3bb6e1b7647f4141775c73deb801fa59db11e49c
zhaojian770627 <zhaojian770627@163.com>
2021-06-13 14:05:32 +0800

    graph add
    Signed-off-by: zhaojian770627 <zhaojian770627@163.com>

---
+++ src/main/java/com/zj/study/algorithm/graph/Graph.java
@@ -0,0 +1,31 @@
+package com.zj.study.algorithm.graph;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Graph<T> {
+	// 是否有向
+	boolean directed = false;
+	Map<String, Vertex<T>> vertexMap = new HashMap<>();
+
+	public void addVertex(String nodeId) {
+		Vertex<T> vertex = new Vertex<T>(nodeId);
+		vertexMap.put(nodeId, vertex);
+	}
+
+	public Vertex<T> getVertex(String nodeId) {
+		return vertexMap.get(nodeId);
+	}
+
+	public void addEdge(String from, String to, T cost) {
+		if (!vertexMap.containsKey(from))
+			addVertex(from);
+
+		if (!vertexMap.containsKey(to))
+			addVertex(to);
+
+		vertexMap.get(from).addNeighbor(to, cost);
+		if (!directed)
+			vertexMap.get(to).addNeighbor(from, cost);
+	}
+}
---
+++ src/main/java/com/zj/study/algorithm/graph/Vertex.java
@@ -0,0 +1,30 @@
+package com.zj.study.algorithm.graph;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Vertex<T> {
+	String id;
+	Map<String, T> adjacent = new HashMap<>();
+
+	public Vertex(String id) {
+		this.id = id;
+	}
+
+	public void addNeighbor(String id, T cost) {
+		adjacent.put(id, cost);
+	}
+
+	public Set<String> getConnections() {
+		return adjacent.keySet();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
