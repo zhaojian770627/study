91852c31b0f4e61ee3433a0cd2e15d5651bf686c
zhaojianc <zhaojianc@yonyou.com>
2021-06-21 16:07:13 +0800

    up
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- src/main/java/com/zj/study/algorithm/graph/tarjan/Tarjan.java
+++ src/main/java/com/zj/study/algorithm/graph/tarjan/Tarjan.java
@@ -41,4 +41,55 @@ public class Tarjan {
 		this.isInStack = new boolean[numOfNode];
 	}
 
+	/**
+	 * 遍历图节点
+	 * 
+	 * @return 强联通分量
+	 */
+	public List<List<Integer>> reverse() {
+		for (int i = 0; i < graph.nodes().size(); i++) {
+			if (DFN[i] == -1)
+				tarjan(i);
+		}
+		return sccsList;
+	}
+
+	/**
+	 * Tarjan算法
+	 * 
+	 * @param curNode
+	 */
+	private void tarjan(int curNode) {
+		// 初始化搜索的次序编号(时间戳)
+		DFN[curNode] = LOW[curNode] = ++index;
+
+		// 入栈
+		stack.push(curNode);
+		isInStack[curNode] = true;
+
+		// 遍历后继节点
+		for (Integer succNode : graph.successors(curNode)) {
+			if (DFN[succNode] == -1) { // 如果没有被访问过,（-1）代表没有被访问过
+				tarjan(succNode); // 递归调用
+				LOW[curNode] = Math.min(LOW[curNode], LOW[succNode]); // 更新所能到的上层节点
+			} else if (isInStack[succNode]) { // 如果在栈中，并且被访问过
+
+				LOW[curNode] = Math.min(LOW[curNode], DFN[succNode]); // 到栈中最上层的节点
+			}
+		}
+
+		// 发现是整个强联通分量子树里的最小根
+		List<Integer> scc = new ArrayList<Integer>();
+		if (LOW[curNode] == DFN[curNode]) {
+			// 出栈
+			int j = -1;
+			while (curNode != j) {
+				j = stack.pop();
+				isInStack[j] = false;
+				scc.add(j);
+			}
+			sccsList.add(scc);
+		}
+
+	}
 }
