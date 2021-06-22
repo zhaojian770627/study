28a87b110405b45bb4425773fbdd03610a2f02ff
zhaojianc <zhaojianc@yonyou.com>
2021-06-22 15:09:55 +0800

    up
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- src/main/java/com/zj/study/algorithm/graph/tarjan/TarjanTest.java
+++ src/main/java/com/zj/study/algorithm/graph/tarjan/TarjanTest.java
@@ -10,23 +10,22 @@ public class TarjanTest {
 
 	public static void main(String[] args) {
 		MutableGraph<Integer> graph = GraphBuilder.directed().incidentEdgeOrder(ElementOrder.stable()).build();
-		graph.putEdge(0, 2);
 		graph.putEdge(0, 1);
-		graph.putEdge(1, 3);
-		graph.putEdge(2, 4);
-		graph.putEdge(2, 3);
-		graph.putEdge(3, 0);
-		graph.putEdge(3, 5);
+		graph.putEdge(1, 2);
+		graph.putEdge(2, 0);
+
+		graph.putEdge(3, 4);
 		graph.putEdge(4, 5);
+		graph.putEdge(5, 3);
+
+		graph.putEdge(2, 4);
 
 		Tarjan tarjan = new Tarjan(graph);
 		List<List<Integer>> result = tarjan.reverse();
 
-		// 校验
-		System.out.println(result.size());
-		System.out.println("[5]====>" + result.get(0).toString());
-		System.out.println("[4]====>" + result.get(1).toString());
-		System.out.println("[1, 3, 2, 0]====>" + result.get(2).toString());
+		for (List<Integer> ints : result) {
+			System.out.println(ints);
+		}
 	}
 
 }
