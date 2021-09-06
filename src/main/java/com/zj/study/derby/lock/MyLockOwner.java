04fe7afae756bc6c3a072eeb3868ec42c9268e63
zhaojianc <zhaojianc@yonyou.com>
2021-09-06 16:15:06 +0800

    up
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- src/main/java/com/zj/study/derby/lock/MyLockOwner.java
+++ src/main/java/com/zj/study/derby/lock/MyLockOwner.java
@@ -4,22 +4,23 @@ import org.apache.derby.iapi.services.locks.LockOwner;
 
 public class MyLockOwner implements LockOwner {
 
+	private boolean noWait;
+	private boolean isNestedOwner;
+	private boolean nestsUnder;
+
 	@Override
 	public boolean noWait() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.noWait;
 	}
 
 	@Override
 	public boolean isNestedOwner() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.isNestedOwner;
 	}
 
 	@Override
 	public boolean nestsUnder(LockOwner other) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.nestsUnder;
 	}
 
 }
