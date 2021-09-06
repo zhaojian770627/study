37f30e9a85e60b244814efec281db41059c3ba3f
zhaojianc <zhaojianc@yonyou.com>
2021-09-06 16:20:44 +0800

    up
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

---
+++ src/main/java/com/zj/study/derby/lock/MyLockObject.java
@@ -0,0 +1,40 @@
+package com.zj.study.derby.lock;
+
+import java.util.Hashtable;
+
+import org.apache.derby.iapi.services.locks.Latch;
+import org.apache.derby.iapi.services.locks.Lockable;
+
+public class MyLockObject implements Lockable {
+
+	@Override
+	public void lockEvent(Latch lockInfo) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean requestCompatible(Object requestedQualifier, Object grantedQualifier) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean lockerAlwaysCompatible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void unlockEvent(Latch lockInfo) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean lockAttributes(int flag, Hashtable<String, Object> attributes) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
