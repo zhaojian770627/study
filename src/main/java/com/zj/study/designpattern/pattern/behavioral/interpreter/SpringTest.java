206fc0df9e685752689e3d17e4ce0bc1c49fcad6
zhaojianc <zhaojianc@yonyou.com>
2021-11-19 17:13:44 +0800

    up
    Signed-off-by: zhaojianc <zhaojianc@yonyou.com>

--- src/main/java/com/zj/study/designpattern/pattern/behavioral/interpreter/SpringTest.java
+++ src/main/java/com/zj/study/designpattern/pattern/behavioral/interpreter/SpringTest.java
@@ -2,6 +2,7 @@ package com.zj.study.designpattern.pattern.behavioral.interpreter;
 
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpression;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 public class SpringTest {
@@ -9,6 +10,7 @@ public class SpringTest {
 	public static void main(String[] args) {
 		ExpressionParser parser = new SpelExpressionParser();
 		Expression expression = parser.parseExpression("100*2+400*1+66");
+		SpelExpression spelExp = (SpelExpression) expression;
 		int result = (int) expression.getValue();
 		System.out.println(result);
 	}
