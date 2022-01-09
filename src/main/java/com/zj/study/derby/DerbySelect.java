9c3189295fee60e562226a035e7d7bedd4340da5
zhaojian770627 <zhaojian770627@163.com>
2022-01-09 21:59:28 +0800

    增加分析文件

--- src/main/java/com/zj/study/derby/DerbySelect.java
+++ src/main/java/com/zj/study/derby/DerbySelect.java
@@ -8,13 +8,14 @@ import java.sql.SQLException;
 
 import org.apache.derby.shared.common.sanity.SanityManager;
 
+
 public class DerbySelect {
 	Connection conn;
 
 	public static void main(String[] args) throws Exception {
-		SanityManager.DEBUG_SET("ClassLineNumbers");
-		SanityManager.DEBUG_SET("DumpClassFile");
-		SanityManager.DEBUG_SET("ByteCodeGenInstr");
+//		SanityManager.DEBUG_SET("ClassLineNumbers");
+//		SanityManager.DEBUG_SET("DumpClassFile");
+//		SanityManager.DEBUG_SET("ByteCodeGenInstr");
 		SanityManager.DEBUG_SET("DumpParseTree");
 		Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
 
